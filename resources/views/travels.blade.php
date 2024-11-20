595ca82ba92be3d16431768a014e23d238178bcd
Mathéo <mattheo.guzman@etu.univ-smb.fr>
2024-11-20 16:42:51 +0000

    modification page sejour

--- resources/views/travels.blade.php
+++ resources/views/travels.blade.php
@@ -9,7 +9,7 @@
 <body>
     <header class="header">
         <div class="logo">
-            <img src="logo.png" alt="Vinotrip Logo">
+            <img src="./images/logo.png" alt="Vinotrip Logo">
             <span>VINOTRIP</span>
         </div>
         <nav class="navbar">
@@ -38,11 +38,26 @@
             <h1>SÉJOURS ŒNOLOGIQUES</h1>
             <h2>Comment choisir son séjour œnologique</h2>
             <p>
-                Personnalisez votre séjour œnologique en fonction de vos envies ! Séjourner au Château dans le Bordelais, réaliser votre propre cuvée en Champagne, découvrir les accords mets & vins en Bourgogne, se relaxer dans un spa au milieu des vignes ou encore survoler la plaine d'Alsace en montgolfière, ...
+                Personnalisez votre séjour œnologique en 
+                fonction de vos envies ! Séjourner au 
+                Château dans le Bordelais, réaliser 
+                votre propre cuvée en Champagne, découvrir 
+                les accords mets & vins en Bourgogne, se 
+                relaxer dans un spa au milieu des vignes ou 
+                encore survoler la plaine d'Alsace en 
+                montgolfière, ...
+                Proposés par thématique – bien-être, gastronomie,
+                 culture ou sport – nos week-ends œnologiques 
+                 sont élaborés pour permettre à tous les publics, 
+                 de l’amateur au néophyte, de trouver leur bonheur.
             </p>
-            <p>
-                Proposés par thématique – bien-être, gastronomie, culture ou sport – nos week-ends œnologiques sont élaborés pour permettre à tous les publics, de l’amateur au néophyte, de trouver leur bonheur.
-            </p>
+        </section>
+        <section class="sejour">
+            <div id="num1">
+                <p>Titre sejour</p>
+                <br>
+                <p>Debut desc sejour</p><p>moyenne avis avec etoiles</p>
+            </div>
         </section>
     </main>
 </body>
