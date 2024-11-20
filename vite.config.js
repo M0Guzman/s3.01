e53cc471031ac800dcee8f457ccb14fd7114af07
Mathéo <mattheo.guzman@etu.univ-smb.fr>
2024-11-20 16:05:57 +0000

    Got scss working

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,19 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    css: {
+        preprocessorOptions: {
+          scss: {
+            additionalData: `@use "resources/scss/global.scss" as *;`,
+          },
+        },
+      },
     plugins: [
         laravel({
             input: [
                 'resources/css/app.css',
                 'resources/js/app.js',
+                'resources/scss/app.scss'
             ],
             refresh: true,
         }),
