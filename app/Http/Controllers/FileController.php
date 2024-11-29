6a6a34ca39b71ea6d920ce6cb6d4872c4e07da13
RedBoxing <redboxing@redboxing.moe>
2024-11-29 11:24:29 +0100

    Add resource route

---
+++ app/Http/Controllers/FileController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Ressource;
+use Illuminate\Support\Facades\Storage;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
+
+class FileController extends Controller
+{
+    // TODO: check permissions
+    public function get_file($id) : BinaryFileResponse {
+        if(Storage::exists($id))
+        {
+            return response()->file(Storage::path($id));
+        } else {
+            return abort(404);
+        }
+    }
+}
