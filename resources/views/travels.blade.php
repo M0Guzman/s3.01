8038faea1a493976f21fb252b2f1923af1ec5e53
Mathéo <mattheo.guzman@etu.univ-smb.fr>
2024-11-29 07:33:01 +0000

    finition

--- resources/views/travels.blade.php
+++ resources/views/travels.blade.php
@@ -121,7 +121,7 @@
             
             <p id="image"> image avec etiquette pour le prix</p>
             <p id="title">{{ mb_substr($sejour->title,0,50,'UTF-8') }}</p>                                        
-            <p id="vignoble">{{ mb_substr($sejour->vineyard_category->name,0,50,'UTF-8') }} {{ $sejour->rating }} étoiles</p>
+            <p id="vignoble">{{ mb_substr($sejour->vineyard_category->name,0,50,'UTF-8') }} @if( $sejour->rating != '') {{ $sejour->rating }} étoiles @endif</p>
             <p id="description">{{ mb_substr($sejour->description,0,50,'UTF-8') }}</p>
             <p id="jours">{{ $sejour->days }} @if( $sejour->days >1)jours 
               @else 
