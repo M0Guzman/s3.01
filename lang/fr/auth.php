168c9039d09277956df5bfd5c9aa75839a0710b2
RedBoxing <redboxing@redboxing.moe>
2024-12-02 23:53:07 +0100

    Added locale translations for error messages

---
+++ lang/fr/auth.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'Identifiants invalides.',
+    'password' => 'Mot de passe incorrecte.',
+    'throttle' => 'Trop de tentatives de connexion. Veuillez resseilliez dans :seconds secondse.',
+
+];
