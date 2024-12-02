2edafa90f0852c1b640acd09926491befcafb67d
Mathéo <mattheo.guzman@etu.univ-smb.fr>
2024-12-02 08:49:08 +0000

    recupere les modeles

---
+++ app/Http/Controllers/TravelController.php
@@ -0,0 +1,92 @@
+<?php
+ 
+namespace App\Http\Controllers;
+ 
+use App\Models\ParticipantCategory;
+use App\Models\Review;
+use Illuminate\Http\Request;
+use App\Models\Travel;
+use App\Models\TravelCategory;
+use App\Models\VineyardCategory;
+use App\Models\TravelHasResource;
+use App\Models\Ressource;
+ 
+class TravelController extends Controller
+{
+ 
+    public function show(Request $request)
+    {
+        
+        
+        $travels = [];
+        
+        
+        
+
+        if (
+            $request->has('vignoble') &&   $request->input('vignoble') != ''|| 
+            $request->has('duree') &&  $request->input('duree') != '' ||
+            $request->has('pour-qui') &&   $request->input('pour-qui') != '' ||
+            $request->has('envie') && $request->input('envie') != '') {
+            $wheres = [];
+            
+            if($request->has('vignoble') && $request->input('vignoble') != '')
+            {
+                $cavignoble = VineyardCategory::where('name', '=', $request->input('vignoble'))->first();
+                
+                array_push($wheres, ['vineyard_category_id', '=', $cavignoble->id]);
+            }
+            if($request->has('duree') && $request->input('duree') != '')
+            {   
+                
+                array_push($wheres, ['days', '=', $request->input('duree')]);
+
+            }
+            if($request->has('pour-qui') && $request->input('pour-qui') != '')
+            {           
+                $caParticipant = ParticipantCategory::where('name','=',$request->input('pour-qui'))->first();
+                
+                array_push($wheres, ['participant_category_id', '=', $caParticipant->id]);
+
+            }
+            if($request->has('envie') && $request->input('envie') != '')
+            {           
+                $caTravel = TravelCategory::where('name','=',$request->input('envie'))->first();
+                
+                array_push($wheres, ['travel_category_id', '=', $caTravel->id]); 
+            }
+
+            foreach($wheres as $where) {
+                if($travels == null) {
+                    $travels = Travel::where($where[0], $where[1], $where[2]);
+                } else {
+                    $travels = $travels->where($where[0], $where[1], $where[2]);
+                }
+            }
+
+
+            if($travels != null) {
+            $travels->leftJoin('reviews', 'reviews.travel_id', '=', 'travel.id')->avg('rating');
+            $travels = $travels->get(['travel.*', 'rating']);
+            
+            }
+            
+            
+                      
+        } else {
+            $travels = Travel::take(1000);
+            $travels->leftJoin('reviews', 'reviews.travel_id', '=', 'travel.id')->avg('rating');
+
+            $travels = $travels->get(['travel.*', 'rating']);
+        }
+
+        
+ 
+            return view('travels', ['vignoble'=>$request->input('vignoble'),'duree'=>$request->input('duree'),'pour_qui'=>$request->input('pour-qui'),'envie'=>$request->input('envie'),'sejours' => $travels]);
+    }
+    public function store(Request $request){
+        $travel = [];
+
+        
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Middleware\FullyVerified;
+use App\Http\Controllers\TravelController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,11 +20,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/travels', [TravelController::class, 'show'])->name('travels.show');
+
+Route::get('/afficher', [TravelController::class,'afficher'])->name('travels.afficher');
 Route::get('/dashboard', function () {
     return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+})->middleware(['auth', FullyVerified::class])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
+Route::middleware(['auth', FullyVerified::class])->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
