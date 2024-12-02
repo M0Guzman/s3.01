e6e725e806d8d5c249b6160442d9106bf3b503c9
Mathéo <mattheo.guzman@etu.univ-smb.fr>
2024-12-02 08:28:26 +0000

    regler les demandes

--- app/Http/Controllers/TravelController.php
+++ app/Http/Controllers/TravelController.php
@@ -19,9 +19,9 @@ class TravelController extends Controller
         
         
         $travels = [];
-        //$travels = Travel::query();
+        
         $page = 0;
-        //dd($request);
+        
 
         if (
             $request->has('vignoble') &&   $request->input('vignoble') != ''|| 
@@ -33,27 +33,27 @@ class TravelController extends Controller
             if($request->has('vignoble') && $request->input('vignoble') != '')
             {
                 $cavignoble = VineyardCategory::where('name', '=', $request->input('vignoble'))->first();
-                //$travels = $travels->where('vineyard_category_id', '=', $cavignoble->id);
+                
                 array_push($wheres, ['vineyard_category_id', '=', $cavignoble->id]);
             }
             if($request->has('duree') && $request->input('duree') != '')
             {   
-                //$travels = $travels->where('days','=', $request->input('duree'));
+                
                 array_push($wheres, ['days', '=', $request->input('duree')]);
 
             }
             if($request->has('pour-qui') && $request->input('pour-qui') != '')
             {           
                 $caParticipant = ParticipantCategory::where('name','=',$request->input('pour-qui'))->first();
-                //$travels = $travels->where('participant_category_id','=',$caParticipant->id);
+                
                 array_push($wheres, ['participant_category_id', '=', $caParticipant->id]);
 
             }
             if($request->has('envie') && $request->input('envie') != '')
             {           
                 $caTravel = TravelCategory::where('name','=',$request->input('envie'))->first();
-                //$travels = $travels->where('travel_category','=',$caTravel->id);
-                array_push($wheres, ['travel_category_id', '=', $caTravel->id]); //erreur array_push() does not accept unknown named parameters
+                
+                array_push($wheres, ['travel_category_id', '=', $caTravel->id]); 
             }
 
             foreach($wheres as $where) {
@@ -68,15 +68,14 @@ class TravelController extends Controller
             if($travels != null) {
             $travels->leftJoin('reviews', 'reviews.travel_id', '=', 'travel.id')->avg('rating');
             $travels = $travels->get(['travel.*', 'rating']);
-            //$travels = $travels->get();
+            
             }
             
-            //->take(10)
-            //->get();
+            
                       
         } else {
             $page = $page +1;
-            $travels = Travel::take(1000);//->offset($page * 10);
+            $travels = Travel::take(1000);
             $travels->leftJoin('reviews', 'reviews.travel_id', '=', 'travel.id')->avg('rating');
 
             $travels = $travels->get(['travel.*', 'rating']);
