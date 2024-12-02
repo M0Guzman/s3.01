92d29c461838e09e8c4391953711bbcdc9a07451
Mathéo <mattheo.guzman@etu.univ-smb.fr>
2024-12-02 08:12:21 +0000

    regler les demande

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,21 +5,3 @@ import Alpine from 'alpinejs';
 window.Alpine = Alpine;
 
 Alpine.start();
-
-document.getElementById('search-button').addEventListener('click', () => {
-    const vignoble = document.getElementById('vignoble').value;
-    const duree = document.getElementById('duree').value;
-    const pourQui = document.getElementById('pour-qui').value;
-    const envie = document.getElementById('envie').value;
-  
-    /*if (!vignoble && !duree && !pourQui && !envie) {
-      alert('Veuillez remplir tous les champs pour effectuer une recherche.');
-      return;
-    }*/
-  
-    // Simuler une recherche ou rediriger vers une page avec les filtres appliqués
-    console.log(`Recherche : Vignoble = ${vignoble}, Durée = ${duree}, Pour qui = ${pourQui}, Envie = ${envie}`);
-    //alert(`Recherche effectuée avec les critères : \nVignoble : ${vignoble}\nDurée : ${duree}\nPour qui : ${pourQui}\nEnvie : ${envie}`);
-  });
-
-  
