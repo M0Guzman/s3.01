7c6a597fff8f09a41d4035da63df9a9436975b19
Votre Nom[ <matteobertauld@gmail.com>
2025-01-06 13:38:45 +0000

    address script

--- resources/views/order_process/address.blade.php
+++ resources/views/order_process/address.blade.php
@@ -15,7 +15,7 @@
 
             <div class=" flex flex-col sm:justify-center items-center sm:pt-0 bg-gray-100 dark:bg-gray-900">
                 <div class="w-full sm:max-w-xl mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
-                    <form method="POST" action="{{ route('order.process.address') }}">
+                    <form method="POST" action="{{ route('order.process.address') }}" class="address-form">
                         @csrf
 
                         <div class="w-full mr-3 mb-4">
@@ -33,56 +33,58 @@
                         <div id="add_address">
                             <div class="mt-4 mb-6">Ou <br> Créer une nouvelle addresse</div>
 
-                            <div class="w-full mr-3 mb-4">
-                                <x-input-label for="address_name" value="Nom de l'addresse" />
-            <x-text-input id="address_name" class="block mt-1 w-full" type="text" name="address_name" autofocus autocomplete="address_name" />
-            <x-input-error :messages="$errors->get('address_name')" class="mt-2" />
-        </div>
+                <div class="w-full mr-3 mb-4">
+                    <x-input-label for="address_name" value="Nom de l'addresse" />
+                    <x-text-input id="address_name" class="block mt-1 w-full" type="text" name="address_name" autofocus autocomplete="address_name" />
+                    <x-input-error :messages="$errors->get('address_name')" class="mt-2" />
+                </div>
 
-        <div class="w-full mr-3 mb-4">
-            <x-input-label for="address_street" value="Rue" />
-            <x-text-input id="address_street" class="block mt-1 w-full" type="text" name="address_street" autofocus autocomplete="address_street" />
-            <x-input-error :messages="$errors->get('address_street')" class="mt-2" />
-        </div>
+                <div class="w-full mr-3 mb-4">
+                    <x-input-label for="address_phone" value="Numéro de téléphone" />
+                    <x-text-input id="address_phone" class="block mt-1 w-full" type="text" name="address_phone" autofocus autocomplete="address_phone" />
+                    <x-input-error :messages="$errors->get('address_phone')" class="mt-2" />
+                </div>
 
-        <div class="w-full mr-3 mb-4">
-            <x-input-label for="address_phone" value="Numéro de téléphone" />
-            <x-text-input id="address_phone" class="block mt-1 w-full" type="text" name="address_phone" autofocus autocomplete="address_phone" />
-            <x-input-error :messages="$errors->get('address_phone')" class="mt-2" />
-        </div>
+                <div class="w-full mr-3 mb-4">
+                    <x-input-label for="address_street" value="Rue" />
+                    <x-text-input id="rue" class="block mt-1 w-full" type="text" name="address_street" autofocus autocomplete="address_street" />
+                    <x-input-error :messages="$errors->get('rue')" class="mt-2" />
+                </div>
 
-        <div class="w-full mr-3 mb-4">
-            <x-input-label for="address_city" value="Ville" />
-            <x-text-input id="address_street" class="block mt-1 w-full" type="text" name="address_city" autofocus autocomplete="address_city" />
-            <x-input-error :messages="$errors->get('address_city')" class="mt-2" />
-        </div>
+                <div class="w-full mr-3 mb-4">
+                    <x-input-label for="address_city" value="ville" />
+                    <x-text-input id="ville" class="block mt-1 w-full" type="text" name="address_city" autofocus autocomplete="address_city" />
+                    <x-input-error :messages="$errors->get('address_city')" class="mt-2" />
+                </div>
 
 
-        <div class="w-full flex flex-row gap-2 mb-4 mt-4 justify-between">
-            <div>
-                <x-input-label for="address_zip" value="Code Postale" />
-                <x-text-input id="address_zip" class="block mt-1 w-full" type="text" name="address_zip" autofocus autocomplete="address_zip" />
-                <x-input-error :messages="$errors->get('address_zip')" class="mt-2" />
+                <div class="w-full flex flex-row gap-2 mb-4 mt-4 justify-between">
+                    <div>
+                        <x-input-label for="address_zip" value="Code Postale" />
+                        <x-text-input id="cp" class="block mt-1 w-full" type="text" name="address_zip" autofocus autocomplete="address_zip" />
+                        <x-input-error :messages="$errors->get('cp')" class="mt-2" />
+                    </div>
+
+                    <div>
+                        <x-input-label for="address" value="Département" />
+                        <x-select id="department" class="block mt-1 w-full" name="address_department" autofocus autocomplete="address_department" list="addressdepartmentlist">
+                            <option selected>Selectionner une département</<option>
+
+                            @foreach ($departments as $department)
+                                <option value="{{ $department->id }}">{{ $department->name }}</option>
+                            @endforeach
+                        </x-select>
+                    <x-input-error :messages="$errors->get('department')" class="mt-2" />
+                </div>
+
             </div>
-
-            <div>
-                <x-input-label for="address" value="Département" />
-            <x-select id="address_department" class="block mt-1 w-full" name="address_department" autofocus autocomplete="address_department" list="addressdepartmentlist">
-                <option selected>Selectionner une département</<option>
-
-                @foreach ($departments as $department)
-                    <option value="{{ $department->id }}">{{ $department->name }}</option>
-                @endforeach
-            </x-select>
-            <x-input-error :messages="$errors->get('address_department')" class="mt-2" />
-        </div>
-
-        </div>
         </div>
 
         <x-primary-button class="">
             Suivant
-            </x-primary-button>
+        </x-primary-button>
         </div>
     </main>
+    <script>const api_url = "{{ route('autocomplete.cities') }}"</script>
+    @vite(['resources/js/address.js'])
 </x-app-layout>
