e61680005953d4af44cb13702340d9d7bc4f782d
Mathéo <mattheo.guzman@etu.univ-smb.fr>
2025-01-12 21:48:35 +0000

    regle pb service vente lien

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -6,7 +6,7 @@
             <!--<a href="">Administrateur</a>
             <a href="">Modérateur</a>-->
             @if(hasRole("marketing_department"))<a href="{{ route("dashboard.vente.Sejour.afficher") }}">Service Marketing</a>@endif
-            @if(hasRole("sales_department") || hasRole("sales_department_director"))<a href="">Service Vente</a>@endif
+            @if(hasRole("sales_department") || hasRole("sales_department_director"))<a href="{{route('commandes_client.show')}}">Service Vente</a>@endif
             @if(hasRole("sales_department_director"))<a href="{{ route('dashboard.vente.homepage.show') }}">Directeur Service Vente</a>@endif
             @if(hasRole("executive"))<a href="{{route('dashboard.dirigeant.validate.Travel') }}">Dirigeant</a>@endif
         </div>
