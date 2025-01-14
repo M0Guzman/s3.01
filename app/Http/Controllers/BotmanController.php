9fb25a339c654e3c6b83caafbdc7d9edf52529df
RedBoxing <redboxing@redboxing.moe>
2025-01-14 10:01:19 +0100

    Make chatbot able to follow conversation

--- app/Http/Controllers/BotmanController.php
+++ app/Http/Controllers/BotmanController.php
@@ -2,21 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Helpers\BotConversation;
+use BotMan\BotMan\BotMan;
+use BotMan\BotMan\Messages\Conversations\InlineConversation;
 use Codewithkyrian\ChromaDB\Facades\ChromaDB;
 use Illuminate\Routing\Controller;
 use Cloudstudio\Ollama\Facades\Ollama;
 use BotMan\BotMan\Drivers\DriverManager;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Log;
 
 class BotmanController extends Controller
 {
-    public function handle()
+    public function handle(Request $request)
     {
+        $user_id = $request->input('userId');
         DriverManager::loadDriver(\BotMan\Drivers\Web\WebDriver::class);
         $botman = app('botman');
 
-        $botman->hears('{message}', function($botman, $message) {
+        $botman->hears('{message}', function(BotMan $botman, string $message) use ($user_id) {
+            $cache = Cache::get("chatbothistory_" . $user_id, []);
             $questionEmbeddings = Ollama::model('snowflake-arctic-embed2')->embeddings($message);
 
             $collection = ChromaDB::getCollection("chatbot");
@@ -46,16 +52,16 @@ class BotmanController extends Controller
 
             array_push($messages, ['role' => 'sytem', 'content' => "***DEBUT DE NOUVELLE CONVERSATION***"]);
             array_push($messages, ['role' => 'system', 'content' => "En tant que modèle linguistique, votre tâche consiste à répondre à toutes les questions posées de manière concise, courte et sincère. Veillez à fournir des réponses diversifiées et informatives afin de maintenir l'intérêt de la conversation. IL FAUT ABSOLUMENT EVITER DE DONNER DES REPONSE SANS PREUVE NOTER CI-DESSUS. Évitez de rester bloqué dans des boucles ou de répéter la même réponse à plusieurs reprises. Vous êtes un assistant pour la société Vinotrip qui vend des séjours oenologique dans toute le france."]);
-            array_push($messages, ['role' => 'user', 'content' => $message]);
 
-Log::debug(json_encode($messages));
-
+            $cache[] = ['role' => 'user', 'content' => $message];
+            $messages = array_merge($messages, $cache);
 
             $llm = Ollama::agent("")->model('mistral')->options(['temperature' => 0.3])->stream(false)->chat($messages);
-
-Log::debug(json_encode($llm));
-
             $botman->reply($llm['message']['content']);
+            $cache[] =  $llm['message'];
+
+            Cache::put('chatbothistory_' . $user_id, $cache, 600); // 10 minutes
+
 
             /*if(count($query->distances) == 0 || $query->distances[0][0] > 170) {
                 $botman->reply("Désoler, je n'ai pas compris votre question.");
